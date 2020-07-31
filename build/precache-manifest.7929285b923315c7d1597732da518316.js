370b29b105ff410132a207f0cf539368dd73300d
Matti Tuomela <matti.tuomela@outlook.com>
2020-07-31 16:19:09 +0300

    heroku kokeilua

---
+++ build/precache-manifest.7929285b923315c7d1597732da518316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dde6b1ff9fb31ef0e872f3ee816e9e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8c3790a7ef9d3199385",
+    "url": "/static/css/main.1c2efc50.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c3790a7ef9d3199385",
+    "url": "/static/js/main.84ecdc8a.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
